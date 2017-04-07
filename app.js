287ec7c4a55eeb5ba6aadbea0f68c04ec386177f
Erik Cucera <Erik Cucera>
2017-04-07 17:38:50 +0200

    Created package.json and app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '5df94e10-7174-44d4-8cb5-963b122554b9',
+    appPassword: 'eREdqUh3Cp3qFHNpLMqsgLF'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Čau, zákazník");
+});
